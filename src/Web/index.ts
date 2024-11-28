c36476bbc8a480febf27f56a4f519137ae873b18
Schmock <rsk.rook@gmail.com>
2024-11-28 13:18:06 +0100

    fixes

--- src/Web/index.ts
+++ src/Web/index.ts
@@ -1,27 +1,17 @@
-
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './Framework/app.module';
-import * as functions from 'firebase-functions';
+import { Entrypoint } from './Entrypoint'
 import * as dotenv from 'dotenv';
-dotenv.config();
 
-export const api = functions.https.onRequest(async (req, res) => {
-    const app = await NestFactory.create(AppModule);
+dotenv.config()
+Entrypoint.set();
+Entrypoint.Instance.boot();
 
-    app.enableCors({
-        origin: [
-            'https://traumaindicator.com',
-            'https://www.traumaindicator.com'
-        ],
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true,
-        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization, api-key'
-    });
+export { Entrypoint }
 
-    const instance = app.getHttpAdapter().getInstance();
-    return instance(req, res);
-});
-
-process.on('uncaughtException', (err) => {
-    console.error('Uncaught Exception:', err);
-});
+/*
+TODO:
+standardize imports and exports
+clean up unused files
+create enneagream page
+and another page for leadership indication
+clean up the api calls and create a general api for usage instead
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './Web/Framework/app.module';
+import { Entrypoint } from './Web/Entrypoint'
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config()
+Entrypoint.set();
+Entrypoint.Instance.boot();
 
-let server: any; // Cache the app instance for reuse
+export { Entrypoint }
 
-export const createNestServer = async () => {
-    if (!server) {
-        const app = await NestFactory.create(AppModule);
-
-        // Enable CORS for specific origins
-        app.enableCors({
-            origin: [
-                'https://traumaindicator.com',
-                'https://www.traumaindicator.com',
-            ],
-            methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-            credentials: true,
-            allowedHeaders:
-                'Origin, X-Requested-With, Content-Type, Accept, Authorization, api-key',
-        });
-
-        const port = 8080;
-        await app.listen(port);
-        console.log(`NestJS server running on port ${port}`);
-
-        // Cache the server instance
-        server = app.getHttpAdapter().getInstance();
-    }
-
-    return server;
-};
-
-// Handle uncaught exceptions globally
-process.on('uncaughtException', (err) => {
-    console.error('Uncaught Exception:', err);
-});
+/*
+TODO:
+standardize imports and exports
+clean up unused files
+create enneagream page
+and another page for leadership indication
+clean up the api calls and create a general api for usage instead
+*/

5dc27c8addb3da9e35e6541d4a0e6de5b71f99f2
Schmock <rsk.rook@gmail.com>
2024-11-28 07:10:05 +0100

    fixes

--- src/Web/index.ts
+++ src/Web/index.ts
@@ -1,17 +1,27 @@
-import { Entrypoint } from './Entrypoint'
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './Framework/app.module';
+import * as functions from 'firebase-functions';
 import * as dotenv from 'dotenv';
+dotenv.config();
 
-dotenv.config()
-Entrypoint.set();
-Entrypoint.Instance.boot();
+export const api = functions.https.onRequest(async (req, res) => {
+    const app = await NestFactory.create(AppModule);
 
-export { Entrypoint }
+    app.enableCors({
+        origin: [
+            'https://traumaindicator.com',
+            'https://www.traumaindicator.com'
+        ],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization, api-key'
+    });
 
-/*
-TODO:
-standardize imports and exports
-clean up unused files
-create enneagream page
-and another page for leadership indication
-clean up the api calls and create a general api for usage instead
-*/
+    const instance = app.getHttpAdapter().getInstance();
+    return instance(req, res);
+});
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+});

187cd7194df6baa74cf4e9494837d49efd90f268
Schmock <rsk.rook@gmail.com>
2024-11-28 05:40:36 +0100

    fixes

--- src/Web/Framework/api-key.middleware.ts
+++ src/Web/Framework/api-key.middleware.ts
@@ -1,28 +1,13 @@
-import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import { Reflector } from '@nestjs/core';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 @Injectable()
 export class ApiKeyMiddleware implements NestMiddleware {
-    constructor(private reflector: Reflector) { }
-
-    use(req: Request, res: Response, next: NextFunction): void {
-        const route = req.route.path;
-        const handler = req.route.stack[0].handle;
-        const headers = req.headers;
-
-        const apiKey = 'bd6a90bd-31ae-5b91-ef48-271ac0f31c84';
-        const key = headers['Authorization'] || headers['authorization']
-
-      
-        //console.log(req.rawHeaders[5]);
-        if (!apiKey || (headers && key !== apiKey)) {
-            throw new UnauthorizedException('Forbidden: Invalid API key');
+    use(req: Request, res: Response, next: NextFunction) {
+        const apiKey = req.headers['api-key'];
+        if (apiKey !== process.env.API_KEY) {
+            return res.status(403).send('Forbidden: Invalid API Key');
         }
-
         next();
     }
 }
--- src/utils/scoring.ts
+++
@@ -1,9 +0,0 @@
-
-export const weightedEuclideanDistance = (profile: any, ideal: any, weights: any = {}) => {
-  return Math.sqrt(Object.keys(profile).reduce((sum, key) => {
-    const diff = profile[key] - ideal[key];
-    return sum + weights[key] * Math.pow(diff, 2);
-  }, 0));
-};
-
-

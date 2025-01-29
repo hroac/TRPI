369ee40fcab88a93ac80ccf4df07d1641206a025
Schmock <rsk.rook@gmail.com>
2025-01-30 00:39:29 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -121,6 +121,7 @@ function App() {
                             <Route path='/sources' element={<Sources/>} />
                             <Route path='/reviews' element={<Reviews/>} />
                             <Route path='/check/:r1/:r2'  element={<TypeCompatibilityChecker/>} />
+                            <Route path='/check'  element={<TypeCompatibilityChecker/>} />
                         </Routes>
                         <Box position={'relative'} top={225}>
                         <Footer />

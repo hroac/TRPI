65f6ad1be1440135dfed9d755b5d581a05b0d7df
Schmock <rsk.rook@gmail.com>
2025-02-19 20:50:52 +0100

    offline load

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 
 // Import any global CSS or assets here
 import './styles/index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -11,3 +12,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration
